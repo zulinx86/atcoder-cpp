a3e226827b663daedc9a5e7c4953f8d5bb98df7d
Takahiro Itazuri <zulinx86@gmail.com>
2021-04-11 18:54:47 +0900

    add b of abc157

---
+++ abc157/b.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define FOR(i,a,b) for (int i = a; i < b; ++i)
+#define FORR(i,a,b) for (int i = b - 1; i >= a; --i)
+#define REP(i,n) FOR(i,0,n)
+#define REPR(i,n) FORR(i,0,n)
+
+template <typename T> bool chmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template <typename T> bool chmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template <typename T1, typename T2> pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first + r.first, l.second + r.second); }
+template <typename T1, typename T2> pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first - r.first, l.second - r.second); }
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T, size_t sz> ostream &operator<<(ostream &os, const array<T, sz> &arr) { os << '['; for (auto v : arr) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &vec) { os << "deq["; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T, typename TH> ostream &operator<<(ostream &os, const unordered_set<T, TH> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const unordered_multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa) { os << '(' << pa.first << ',' << pa.second << ')'; return os; }
+template <typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+template <typename TK, typename TV, typename TH> ostream &operator<<(ostream &os, const unordered_map<TK, TV, TH> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+
+#define DEBUG
+
+#ifdef DEBUG
+const string COLOR_RESET = "\033[0m", BRIGHT_GREEN = "\033[1;32m", BRIGHT_RED = "\033[1;31m", BRIGHT_CYAN = "\033[1;36m", NORMAL_CROSSED = "\033[0;9;37m", RED_BACKGROUND = "\033[1;41m", NORMAL_FAINT = "\033[0;2m";
+#define dbg(x) cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << " in " << __FILE__ << ")" << COLOR_RESET << endl
+#else
+#define dbg(x) (x)
+#endif
+
+
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	
+	int A[3][3];
+	int N;
+	int b;
+
+	REP(i, 3) {
+		REP(j, 3) {
+			cin >> A[i][j];
+		}
+	}
+
+	cin >> N;
+	REP(n, N) {
+		cin >> b;
+		REP(i, 3) {
+			REP(j, 3) {
+				if (A[i][j] == b) A[i][j] = -1;
+			}
+		}
+	}
+
+	bool ans = false;
+	bool check;
+	REP(i, 3) {
+		check = true;
+		REP(j, 3) {
+			if (A[i][j] != -1) check = false;
+		}
+		if (check) ans = true;
+	}
+
+	REP(i, 3) {
+		check = true;
+		REP(j, 3) {
+			if (A[j][i] != -1) check = false;
+		}
+		if (check) ans = true;
+	}
+
+	check = true;
+	REP(i, 3) {
+		if (A[i][i] != -1) check = false;
+	}
+	if (check) ans = true;
+
+	check = true;
+	REP(i, 3) {
+		if (A[2 - i][i] != -1) check = false;
+	}
+	if (check) ans = true;
+
+	if (ans) cout << "Yes" << endl;
+	else cout << "No" << endl;
+
+	return 0;
+}
