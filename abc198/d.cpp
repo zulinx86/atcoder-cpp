06a774c9dff9accb4fbdd39e6d6230bbde25ffd1
Takahiro Itazuri <zulinx86@gmail.com>
2021-04-12 09:33:21 +0900

    add a - e of abc198

---
+++ abc198/d.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define FOR(i,a,b) for (int i = a; i < b; ++i)
+#define FORR(i,a,b) for (int i = b - 1; i >= a; --i)
+#define REP(i,n) FOR(i,0,n)
+#define REPR(i,n) FORR(i,0,n)
+
+template <typename T> bool chmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template <typename T> bool chmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template <typename T1, typename T2> pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first + r.first, l.second + r.second); }
+template <typename T1, typename T2> pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first - r.first, l.second - r.second); }
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T, size_t sz> ostream &operator<<(ostream &os, const array<T, sz> &arr) { os << '['; for (auto v : arr) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &vec) { os << "deq["; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T, typename TH> ostream &operator<<(ostream &os, const unordered_set<T, TH> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const unordered_multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa) { os << '(' << pa.first << ',' << pa.second << ')'; return os; }
+template <typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+template <typename TK, typename TV, typename TH> ostream &operator<<(ostream &os, const unordered_map<TK, TV, TH> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+
+#define DEBUG
+
+#ifdef DEBUG
+const string COLOR_RESET = "\033[0m", BRIGHT_GREEN = "\033[1;32m", BRIGHT_RED = "\033[1;31m", BRIGHT_CYAN = "\033[1;36m", NORMAL_CROSSED = "\033[0;9;37m", RED_BACKGROUND = "\033[1;41m", NORMAL_FAINT = "\033[0;2m";
+#define dbg(x) cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << " in " << __FILE__ << ")" << COLOR_RESET << endl
+#else
+#define dbg(x) (x)
+#endif
+
+string S[3];
+string T;
+map<char, ll> mp;
+
+ll val(string S) {
+	ll ret = 0;
+	REP(i, S.size()) ret = ret * 10 + mp[S[i]];
+	return ret;
+}
+
+bool solve(vector<ll> v) {
+	for (int i = 0; i < T.size(); ++i) mp[T[i]] = v[i];
+
+	REP(i, 3) {
+		if (mp[S[i][0]] == 0) return false;
+	}
+
+	ll v0 = val(S[0]), v1 = val(S[1]), v2 = val(S[2]);
+	if (v0 + v1 == v2) {
+		cout << v0 << endl << v1 << endl << v2 << endl;
+		return true;
+	}
+
+	return false;
+}
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+
+	REP(i, 3) {
+		cin >> S[i];
+		T += S[i];
+	}
+	sort(T.begin(), T.end());
+	T.erase(unique(T.begin(), T.end()), T.end());
+	
+	if (T.size() >= 11) {
+		cout << "UNSOLVABLE" << endl;
+		return 0;
+	}
+
+	vector<ll> v;
+	for (ll i = 0; i < 10; ++i) v.push_back(i);
+	do {
+		if (solve(v)) return 0;
+	} while(next_permutation(v.begin(), v.end()));
+
+	cout << "UNSOLVABLE" << endl;
+	return 0;
+}
