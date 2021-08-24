44d13f7f0206180091e20eb19f278074ce983ee4
Takahiro Itazur <zulinx86@gmail.com>
2021-08-24 10:49:02 +0900

    update macros

--- template/main.cpp
+++ template/main.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-using uint = unsigned int;
+using ui = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
 
-#define FOR(i,a,b) for (int i = a; i < b; ++i)
-#define FORR(i,a,b) for (int i = b - 1; i >= a; --i)
-#define REP(i,n) FOR(i,0,n)
-#define REPR(i,n) FORR(i,0,n)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 
 template <typename T> bool chmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
 template <typename T> bool chmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
