252655e6ce3c1dc9ae8a14e069420783e938a167
Takahiro Itazur <zulinx86@gmail.com>
2021-09-12 11:44:27 +0900

    add a - d of abc191

---
+++ abc191/d.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ui = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+#define fi first
+#define se second
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+template <typename T> bool chmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template <typename T> bool chmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template <typename T1, typename T2> pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first + r.first, l.second + r.second); }
+template <typename T1, typename T2> pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first - r.first, l.second - r.second); }
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T, size_t sz> ostream &operator<<(ostream &os, const array<T, sz> &arr) { os << '['; for (auto v : arr) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &vec) { os << "deq["; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T, typename TH> ostream &operator<<(ostream &os, const unordered_set<T, TH> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const unordered_multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa) { os << '(' << pa.first << ',' << pa.second << ')'; return os; }
+template <typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+template <typename TK, typename TV, typename TH> ostream &operator<<(ostream &os, const unordered_map<TK, TV, TH> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+
+#define DEBUG
+
+#ifdef DEBUG
+const string COLOR_RESET = "\033[0m", BRIGHT_GREEN = "\033[1;32m", BRIGHT_RED = "\033[1;31m", BRIGHT_CYAN = "\033[1;36m", NORMAL_CROSSED = "\033[0;9;37m", RED_BACKGROUND = "\033[1;41m", NORMAL_FAINT = "\033[0;2m";
+#define dbg(x) cerr << BRIGHT_CYAN << #x << COLOR_RESET << " = " << (x) << NORMAL_FAINT << " (L" << __LINE__ << " in " << __FILE__ << ")" << COLOR_RESET << endl
+#else
+#define dbg(x) (x)
+#endif
+
+const ll unit = 10000;
+
+ll in() {
+	double x;
+	cin >> x;
+	x *= unit;
+	return round(x);
+}
+
+bool ok(ll dx, ll dy, ll z) {
+	return dx * dx + dy * dy <= z * z;
+}
+
+ll count(ll x, ll y, ll z, ll lim) {
+	ll l = 0, r = 1;
+	ll res = 0;
+	for (ll i = (z + unit) / unit * unit; i >= lim; i -= unit) {
+		while (ok(x - l * unit, i - y, z)) --l;
+		while (ok(r * unit - x, i - y, z)) ++r;
+		res += r - l - 1;
+	}
+	return res;
+}
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+
+	ll x, y, z;
+	x = in(); y = in(); z = in();
+	x %= unit;
+	y %= unit;
+
+	ll ans = count(x, y, z, unit) + count(x, -y, z, 0);
+	cout << ans << endl;
+
+	return 0;
+}
